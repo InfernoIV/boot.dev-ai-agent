964c83132c67e4fbcc54a0a817007625e3e8ba97
Joost <github@joostnieuweboer.nl>
2025-06-12 14:02:51 +0000

    Added verbose option

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # boot.dev-ai-agent
+source venv/bin/activate
 python3 main.py
+python3 main.py "What is the meaning of life?"
--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 import os, sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 
+
+
+
 def main():
+    #print(f"Args: '{sys.argv[1:]}'")
     if len(sys.argv) < 2:
         sys.exit("No arguments!")
-    elif len(sys.argv) > 2: 
+    elif len(sys.argv) > 3: 
         sys.exit(f"Too many arguments! {sys.argv[1:]}")
-    else:
+    else:  
+        optional_argument = ""     
+        if len(sys.argv) > 2:
+            optional_argument = sys.argv[2]
+        verbose = optional_argument.lower() == "--verbose"
         #print(f"Input: '{sys.argv[1]}'")
         #post the question
-        handle_assignment(sys.argv[1])
+        handle_assignment(sys.argv[1],verbose)
         pass
     return
 
 
-def handle_assignment(command):
+def handle_assignment(user_prompt, verbose):
     #constant
     model = "gemini-2.0-flash-001"
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+
     #post message and get response
     response = client.models.generate_content(
         model=model,#'gemini-2.0-flash-001', 
-        contents=command#"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=messages#"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     )
+
     #print response
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     #
     return
 
